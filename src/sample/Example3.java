87f36c0de42460fd07a049f2f8397cc1181dba4c
Sudhi <sudhimama66@gmail.com>
2022-06-27 15:16:21 +0530

    commit missed file

--- src/sample/Example3.java
+++ src/sample/Example3.java
@@ -5,7 +5,10 @@ public class Example3 {
 	public static void main(String[] args) {
 		int nfirstnumber=20,nsecondnumber=30,sum;
 		sum=nfirstnumber+nsecondnumber;
+		System.out.println("welcome to java platform");
+		System.out.println("perform your first task");
 		System.out.println("sum of two numbers :" + sum);
+		
 	}
 
 }
