a7b32f6d2dce9eaa1f4b36d39547529bd3ea0a46
Sudhi <sudhimama66@gmail.com>
2022-06-27 17:23:28 +0530

    added diff

--- src/sample/Example3.java
+++ src/sample/Example3.java
@@ -3,8 +3,9 @@ package sample;
 public class Example3 {
 
 	public static void main(String[] args) {
-		int nfirstnumber=20,nsecondnumber=30,sum;
+		int nfirstnumber=20,nsecondnumber=30,sum,diff;
 		sum=nfirstnumber+nsecondnumber;
+		diff=nsecondnumber-nfirstnumber;
 		System.out.println("welcome to java platform");
 		System.out.println("perform your first task");
 		System.out.println("sum of two numbers :" + sum);
