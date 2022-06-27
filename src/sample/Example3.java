076e42d4f5e146d1dcbb2171482154483744a46d
Sudhi <sudhimama66@gmail.com>
2022-06-27 12:14:54 +0530

    Fourth commit

---
+++ src/sample/Example3.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		int nfirstnumber=20,nsecondnumber=30,sum;
+		sum=nfirstnumber+nsecondnumber;
+		System.out.println("sum of two numbers :" + sum);
+	}
+
+}
